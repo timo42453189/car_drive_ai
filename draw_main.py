f16abd09fec0d159ef196f151e9b8ead44005048
timo42453189 <timo.pickelmann@gmail.com>
2023-04-29 11:45:36 +0200

    Add files via upload

--- draw_main.py
+++ draw_main.py
@@ -1,14 +1,26 @@
 import pygame
-from drawing import Draw
-import time
-
+from drawing import Draw, Button
+import os
+import wget
 
 def setup():
-    global WIN, fill_color
+    global WIN, fill_color, buttons
+    buttons = []
     WIDTH = 800
     HEIGHT = 400
     WIN = pygame.display.set_mode((WIDTH, HEIGHT))
     fill_color = (255, 255, 255)
+    
+    try:
+        SAVE = pygame.image.load(os.path.join("save.png"))
+    except:
+        save_url = "https://raw.githubusercontent.com/timo42453189/car_drive_ai_files/main/save.png"
+        wget.download(save_url)
+        SAVE = pygame.image.load(os.path.join("save.png"))
+	
+    SAVE = pygame.transform.scale(SAVE, (30, 30))
+    save = Button(WIN, SAVE, "save", 760, 10)
+    buttons.append(save)
 
 
 circles = []
@@ -29,13 +41,18 @@ def main():
 			if drawing:
 				draw = Draw(WIN, mouse_pos[0], mouse_pos[1], (0, 117, 255), 20)
 				circles.append(draw)
-
+				
 		for circle in circles:
 			circle.draw()
-
-		keys = pygame.key.get_pressed()
-		if keys[pygame.K_k]:
-			pygame.image.save(WIN, 'track.png' )
-
+		
+		for button in buttons:
+			button.show()
+			mouse_hit = button.hit_mouse()
+			if mouse_hit:
+				name = button.get_name()
+				if name == "save":
+					pygame.image.save(WIN, 'track.png' )
+					exit()
+			
 		pygame.display.update()
 
